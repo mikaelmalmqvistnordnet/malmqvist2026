4859a26687e4a1f52fe33aa4d1e2b92262786548
Mikael Malmqvist <mikael@malmqvist.it>
2021-02-27 21:19:57 +0100

    Tweak header

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -4,11 +4,15 @@ import { Link } from '@reach/router';
 import './index.css';
 
 const Navigation: React.FC = () => (
-  <nav className="navigation">
-    <Link to="/">Home</Link>
-    <Link to="profile">Profile</Link>
-    <Link to="contact">Contact</Link>
-  </nav>
+  <header>
+    <nav className="navigation">
+      <Link to="/" className="navigation-home">
+        Mikael Malmqvist
+      </Link>
+      <Link to="profile">Profile</Link>
+      <Link to="contact">Contact</Link>
+    </nav>
+  </header>
 );
 
 export default Navigation;
