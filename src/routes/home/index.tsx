bce7dcef03664020c32fb22698800caefa48f3a8
Mikael Malmqvist <mikael@malmqvist.it>
2021-02-28 08:58:56 +0100

    Move section and paragraph

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -8,24 +8,24 @@ import './index.css';
 const Home: React.FC<RouteComponentProps> = (props) => (
   <div className="home">
     <h1>Hi there 👋</h1>
-    <p>
+    <section>
       <h2 aria-label="Career bio">👨‍💻</h2>
-      <section>
+      <p>
         My name is Mikael and I’m a frontend engineer based in Stockholm,
         Sweden. I currently work as a consultant at{' '}
         <a href="https://www.netlight.com/" target="_blank" rel="noreferrer">
           Netlight Consulting AB
         </a>{' '}
         helping our clients build user-friendly and accessible web applications.
-      </section>
-    </p>
-    <p>
+      </p>
+    </section>
+    <section>
       <h2 aria-label="Education bio">🎓</h2>
-      <section>
-        I’ve got a bachelor’s in software engineering and a master’s in
+      <p>
+        s I’ve got a bachelor’s in software engineering and a master’s in
         interaction design from Chalmers University of Technology.
-      </section>
-    </p>
+      </p>
+    </section>
   </div>
 );
 
