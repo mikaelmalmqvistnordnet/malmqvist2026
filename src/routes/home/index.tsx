f03249dd7e150f4716c93d697fcdc62885552202
Mikael Malmqvist <mikael@malmqvist.it>
2021-02-28 17:21:40 +0100

    Clean up styling and html slightly

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -3,30 +3,26 @@
 import { RouteComponentProps } from '@reach/router';
 import React from 'react';
 
-import './index.css';
-
 const Home: React.FC<RouteComponentProps> = (props) => (
-  <div className="home">
-    <h1>Hi there 👋</h1>
-    <section>
-      <h2 aria-label="Career bio">👨‍💻</h2>
-      <p>
-        My name is Mikael and I’m a frontend engineer based in Stockholm,
-        Sweden. I currently work as a consultant at{' '}
-        <a href="https://www.netlight.com/" target="_blank" rel="noreferrer">
-          Netlight Consulting AB
-        </a>{' '}
-        helping clients build user-friendly and accessible web applications.
-      </p>
-    </section>
-    <section>
-      <h2 aria-label="Education bio">🎓</h2>
-      <p>
-        I’ve got a bachelor’s in software engineering and a master’s in
-        interaction design from Chalmers University of Technology.
-      </p>
-    </section>
-  </div>
+  <>
+    <div className="page">
+      <h1>Hi there 👋</h1>
+      <section>
+        <p>
+          My name is Mikael and I’m a frontend engineer based in Stockholm,
+          Sweden. I currently work as a consultant at{' '}
+          <a href="https://www.netlight.com/" target="_blank" rel="noreferrer">
+            Netlight Consulting AB
+          </a>{' '}
+          helping clients build user-friendly and accessible web applications.
+        </p>
+        <p>
+          I’ve got a bachelor’s in software engineering and a master’s in
+          interaction design from Chalmers University of Technology.
+        </p>
+      </section>
+    </div>
+  </>
 );
 
 export default Home;
