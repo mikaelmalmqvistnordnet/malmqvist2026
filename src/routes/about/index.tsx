08c19d0db6de2a04b5254d5502aff3bacac1d624
Mikael Malmqvist <mikael@malmqvist.it>
2021-10-17 13:06:53 +0200

    Restyle app using tailwind

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -3,22 +3,24 @@
 import { RouteComponentProps } from '@reach/router';
 import React from 'react';
 
-import './index.css';
-
 const skills: string[] = [
   'JavaScript',
   'TypeScript',
   'React',
   'HTML',
   'CSS',
-  'CSS animations',
   'FelaJS',
+  'Emotion',
+  'Tailwind CSS',
   'React context API',
+  'Redux',
+  'Redux-Saga',
   'MobX',
   'Immer',
   'NodeJs',
   'Jest',
   'React testing library',
+  'Enzyme',
   'Cypress',
   'Webpack',
   'Docker',
@@ -30,29 +32,32 @@ const skills: string[] = [
 ];
 
 const About: React.FC<RouteComponentProps> = (props) => (
-  <div className="page">
+  <div className="text-left max-w-3xl pb-1 ">
     <h1>About me</h1>
     <section>
       <p>
-        I’ve got about 6 years experience of web development, and have worked
-        professionally as a frontend engineer for roughly 3 years, mainly as a
-        consultant. I’m also academically trained and got work experience within
-        UX and interaction design. I believe this has given me as a developer
-        solid understanding of concepts such as usability, information
+        I’ve got about 6 years experience of web development, and have worked professionally as a
+        frontend engineer for roughly 3.5 years, mainly as a consultant. I’m also academically
+        trained and got work experience within UX and interaction design. I believe this has given
+        me as a developer solid understanding of concepts such as usability, information
         architecture, prototyping and accessibility.
       </p>
       <p>
-        While frontend with JavaScript is my strong suit, I’ve got good
-        understanding of backend development using NodeJs and also know my way
-        around languages such as Java, C# and Python.
+        While frontend with JavaScript is my strong suit, I’ve got good understanding of backend
+        development using NodeJs and also know my way around languages such as Java, C# and Python.
       </p>
       <p>
-        Besides technical know-how, I’ve also got experience in agile coaching
-        and project management.
+        Besides technical know-how, I’ve also got experience in agile coaching and project
+        management.
       </p>
-      <p>Here are a few skills I posses:</p>
+      <p className="mt-3">Here are a few skills I posses:</p>
       {skills.map((s) => (
-        <span className="skill">{s}</span>
+        <span
+          className="inline-block bg-gray-200 bg-opacity-20 py-1 px-2 m-1 text-sm rounded-3xl"
+          key={s}
+        >
+          {s}
+        </span>
       ))}
     </section>
   </div>
--- src/routes/contact/index.tsx
+++ src/routes/contact/index.tsx
@@ -3,27 +3,31 @@ import { RouteComponentProps } from '@reach/router';
 import React from 'react';
 import { FaLinkedin, FaEnvelope } from 'react-icons/fa';
 
-import './index.css';
-
 const Contact: React.FC<RouteComponentProps> = (props) => (
-  <div className="page">
+  <div className="text-left max-w-3xl pb-1">
     <h1>Contact</h1>
     <section>
       <p>
-        The easiest way to get in touch with me is via email, but you can also
-        reach me via LinkedIn.
+        The easiest way to get in touch with me is via email, but you can also reach me via
+        LinkedIn.
       </p>
     </section>
-    <section className="contact-info">
-      <a href="mailto:mikael@malmqvist.it" target="_blank" rel="noreferrer">
-        <FaEnvelope /> mikael@malmqvist.it
+    <section className="flex flex-col">
+      <a
+        href="mailto:mikael@malmqvist.it"
+        target="_blank"
+        rel="noreferrer"
+        className="flex items-center focus:underline hover:underline"
+      >
+        <FaEnvelope className="mr-1" /> mikael@malmqvist.it
       </a>
       <a
         href="https://www.linkedin.com/in/mikael-malmqvist-579b70a9/"
         target="_blank"
         rel="noreferrer"
+        className="flex items-center focus:underline hover:underline"
       >
-        <FaLinkedin /> LinkedIn
+        <FaLinkedin className="mr-1" /> LinkedIn
       </a>
     </section>
   </div>
