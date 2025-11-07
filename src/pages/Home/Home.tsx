27ba9aa0b1258c6245f7c30332b6fb8cc4c6b9f8
Mikael Malmqvist <mikael.malmqvist@nordnet.se>
2025-11-07 08:29:11 +0100

    Fix

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,21 +7,21 @@ import { PageWrapper } from '../../components/PageWrapper';
 const Home = () => {
   const [pw, setPw] = useState('');
 
-  // if (pw !== 'testmikael123') {
-  //   return (
-  //     <div className="mb-6">
-  //       <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-  //         Password
-  //       </label>
-  //       <input
-  //         className="shadow appearance-none border border-grey-500 rounded max-w-[250px] w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-  //         id="password"
-  //         type="password"
-  //         onChange={(e) => setPw(e.target.value)}
-  //       />
-  //     </div>
-  //   );
-  // }
+  if (pw !== 'testmikael123') {
+    return (
+      <div className="mb-6">
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+          Password
+        </label>
+        <input
+          className="shadow appearance-none border border-grey-500 rounded max-w-[250px] w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          id="password"
+          type="password"
+          onChange={(e) => setPw(e.target.value)}
+        />
+      </div>
+    );
+  }
 
   return (
     <PageWrapper>
